1acab317a0b782926a01a4d8e1b2a4004453d525
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-05 15:49:21 +0000

    code loop

---
+++ assets/Javascript/code_loop.js
@@ -0,0 +1,16 @@
+for ( counter = 1 ; counter <= 10; counter = counter + 1 )
+
+{
+document.write ("<p>",counter, " x 6 = " ,counter*6," </p>" )
+
+}
+let multiplyBy = prompt ("Please enter a number:")
+for ( counter = 1 ; counter <= 10; counter = counter + 1 );
+
+/*let counter =1;
+while (counter <=10 )
+{ document.write(counter);
+....
+....
+counter = counter+1;
+}*/
