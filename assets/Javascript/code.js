30f61a6e83835e559b95ca142525725aaa853afb
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-05 15:47:50 +0000

    code new and loop

---
+++ assets/Javascript/code.js
@@ -0,0 +1,33 @@
+window.alert("Hello World!");
+document.write("<center><h1>Hello World ! </h1></center>");
+document.write("<center><h2>Hello World ! </h2></center>");
+document.write("<center><p>Hello World ! </p></center>");
+
+/*define the varibles*/
+let x=3;
+let y=6;
+let z=8;
+/*calculate*/
+let A = x+y+z;
+let B= (2*x)+(3*z);
+let C= z-x;
+let D= A+B+C;
+
+console.log("A = ", A);
+console.log("B = ", B);
+console.log("C = ", C);
+
+window.alert(A);
+window.alert(B);
+window.alert(C);
+
+window.document.write(`<p> x = ${x}</p>`);
+window.document.write("<p> y = " +y+ "</p>");
+window.document.write("<p> z = " ,z, "</p>");
+
+document.write("<p>A = " , A ,"</p>");
+document.write("<p>B = " , B ,"</p>");
+document.write("<p>C = " , C ,"</p>");
+
+document.write("<p>C = " , C ,"</p>");
+
---
+++ assets/Javascript/code_new.js
@@ -0,0 +1,5 @@
+document.write("<table border=2><tr><td> D = ", D, "</td></tr></table>");
+
+fname=prompt("What is your name");
+document.write(fname);
+
