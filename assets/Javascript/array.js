a550e945c7b37a03d8df6ff8f18cf0b05453a459
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-07 10:46:12 +0000

    array added

---
+++ assets/Javascript/array.js
@@ -0,0 +1,5 @@
+let students = ["Olena", "Igor", "Komal", "Mila", "Abdul"] ;
+
+document.write ("<p>", students[0], "</p>");
+document.write ("<p>", students[1], "</p>");
+document.write ("<p>", students[4], "</p>");
