27c65019a5a1c4f8c231d529d6559940095840b8
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-07 11:04:16 +0000

    index

--- assets/Javascript/array.js
+++ assets/Javascript/array.js
@@ -6,3 +6,6 @@ document.write ("<p>", students[4], "</p>");
 
 let studentCount = students.length;
 document.write ("<p> , Number of elements = ",studentCount, "</p>");
+
+for (let index = 0 ; index < studentCount ; index = index + 1);
+document.write ("<p> , Students = ", [index] , "</p>");
