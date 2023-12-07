27124b54ffe25a251ee3f8a225f57307aef1a9cf
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-07 10:15:40 +0000

    all added

--- assets/Javascript/code_loop.js
+++ assets/Javascript/code_loop.js
@@ -6,6 +6,19 @@ document.write ("<p>",counter, " x 6 = " ,counter*6," </p>" )
 }
 let multiplyBy = prompt ("Please enter a number:")
 for ( counter = 1 ; counter <= 10; counter = counter + 1 );
+{document.write("<p>", counter, "X", multiplyBy, "-", counter*miltiplyBy, "</p>")}
+
+/*
+let multiplyBy = prompt ("Please enter a number:")
+for ( counter = 10 ; counter >=1; counter = counter - 1 );
+{document.write("<p>", counter, "X", multiplyBy, "-", counter*miltiplyBy, "</p>")}*/
+
+/* table
+let multiplyBy = prompt ("Please enter a number:")
+document.write("<table border=1>")
+for ( counter = 1 ; counter <= 10; counter = counter + 1 );
+{document.write("<tr><td>", counter, "</td><td>", multiplyBy, "</td><td>", counter*miltiplyBy, "</td></tr>")}
+document.write("</table>")*/
 
 /*let counter =1;
 while (counter <=10 )
---
+++ assets/Javascript/prompt.js
@@ -0,0 +1,7 @@
+let firstNum = prompt ("Please enter a number:")
+let secondNum = prompt ("Please enter a number:")
+
+document.write("br", firstNum, "x", secondNum, "=", firstNum*secondNum);
+document.write("br", firstNum, "/", secondNum, "=", firstNum/secondNum);
+document.write("br", firstNum, "-", secondNum, "=", firstNum-secondNum);
+document.write("br", firstNum, "+", secondNum, "=", firstNum+secondNum);
