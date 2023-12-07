01bed63525359c3e58a08b2307c72fc978dcd36b
Pilotil76 <147519160+Pilotil76@users.noreply.github.com>
2023-12-07 10:54:41 +0000

    count of elements

--- assets/Javascript/array.js
+++ assets/Javascript/array.js
@@ -3,3 +3,6 @@ let students = ["Olena", "Igor", "Komal", "Mila", "Abdul"] ;
 document.write ("<p>", students[0], "</p>");
 document.write ("<p>", students[1], "</p>");
 document.write ("<p>", students[4], "</p>");
+
+let studentCount = students.length;
+document.write ("<p> , Number of elements = ",studentCount, "</p>");
--- assets/Javascript/prompt.js
+++ assets/Javascript/prompt.js
@@ -1,7 +1,7 @@
 let firstNum = prompt ("Please enter a number:")
 let secondNum = prompt ("Please enter a number:")
 
-document.write("br", firstNum, "x", secondNum, "=", firstNum*secondNum);
-document.write("br", firstNum, "/", secondNum, "=", firstNum/secondNum);
-document.write("br", firstNum, "-", secondNum, "=", firstNum-secondNum);
-document.write("br", firstNum, "+", secondNum, "=", firstNum+secondNum);
+document.write("<p>", firstNum, "x", secondNum, "=", firstNum*secondNum ,"</p>");
+document.write("<p>", firstNum, "/", secondNum, "=", firstNum/secondNum ,"</p>");
+document.write("<p>", firstNum, "-", secondNum, "=", firstNum-secondNum ,"</p>");
+document.write("<p>", firstNum, "+", secondNum, "=", firstNum+secondNum ,"</p>");
